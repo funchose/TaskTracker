4859b3ed6b0b395ee1723c982f96d11704727299
Anna Popova <foumart14@gmail.com>
2024-10-06 14:32:29 +0300

    add exception handling

--- src/main/java/org/study/tracker/exceptions/GlobalExceptionHandler.java
+++ src/main/java/org/study/tracker/exceptions/GlobalExceptionHandler.java
@@ -1,19 +1,19 @@
 package org.study.tracker.exceptions;
 
-import java.util.stream.Collectors;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.context.support.DefaultMessageSourceResolvable;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import org.springframework.web.bind.MethodArgumentNotValidException;
+import java.util.stream.Collectors;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
